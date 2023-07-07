81f7071805587c4ee7d7d85a36abc7d769572673
Grégory Viguier <git@screenfeed.fr>
2023-07-07 14:38:58 +0200

    Fixed `build.sh`

--- bin/build.sh
+++ bin/build.sh
@@ -4,22 +4,20 @@
 # $1 string Whether to run composer install or update. '-u' or '--update' to update, anything else to install.
 # $2 string '--no-npm' to not run npm.
 
-if [[ "$1" = "-u" ]] || [[ "$1" = "--update" ]]; then
-	local COMPOSER_COMMAND='update'
-else
-	local COMPOSER_COMMAND='install'
-fi
-
 echo "Installing PHP packages..."
-rm -rf vendor/ # Make sure to remove all traces of development dependencies.
-composer $COMPOSER_COMMAND # Need update to ensure to have the latest version of the dependencies.
+# Make sure to remove all traces of development dependencies.
+rm -rf vendor
+# Update/Install to ensure to have the latest version of the dependencies.
+if [[ "$1" = "-u" ]] || [[ "$1" = "--update" ]]; then
+	composer update
+else
+	composer install
+fi
 
 if [[ '--no-npm' != $2 ]]; then
 	echo "Running build..."
-	npm install && npm run build # minify js and css files.
+	# Minify js and css files.
+	npm install && npm run build
 fi
 
 echo "Build done!"
-
-echo "Cleanup." # Discard composer.lock and package-lock.json changings to ensure they'll never be pushed on repository.
-git checkout -- composer.lock package-lock.json
