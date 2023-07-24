617439ea2d7f0682f97d5bfba4c7053969f62628
Grégory Viguier <git@screenfeed.fr>
2023-07-24 13:53:10 +0200

    No need of `setUp()`

--- Fixtures/DummyTestCase.php
+++ Fixtures/DummyTestCase.php
@@ -9,26 +9,12 @@
 namespace WP_Syntex\Polylang_Phpunit\Fixtures;
 
 use WP_Syntex\Polylang_Phpunit\Integration\TestCaseTrait;
-use Yoast\PHPUnitPolyfills\TestCases\TestCase;
+use WP_UnitTestCase;
 
 /**
  * Dummy test case so PHPStan can analyze TestCaseTrait.
  */
-class DummyTestCase extends TestCase {
+class DummyTestCase extends WP_UnitTestCase {
 
 	use TestCaseTrait;
-
-	/**
-	 * Sets up the fixture, for example, open a network connection.
-	 *
-	 * @return void
-	 */
-	protected function setUp(): void {
-		// Must be set before calling `parent::set_up()`.
-		$this->activePlugins = [
-			'wp-all-import-pro/wp-all-import-pro.php',
-		];
-
-		parent::setUp();
-	}
 }
