dac7b3d3a311943299d0b7d76667d718856cf9cf
Grégory Viguier <Screenfeed@users.noreply.github.com>
2024-11-26 12:09:23 +0100

    Apply suggestions from code review
    Co-authored-by: Hugo Drelon <69580439+Hug0-Drelon@users.noreply.github.com>

--- bin/generic-download-tools.sh
+++ bin/generic-download-tools.sh
@@ -17,8 +17,8 @@ getContent() {
 # $2 string Destination path to download the file to.
 download() {
 	if [[ `which curl` ]]; then
-		curl -sL "$1" > "$2";
+		curl -sL --proto '=https' --max-redirs 2 "$1" > "$2";
 	elif [[ `which wget` ]]; then
-		wget -nv -O "$2" "$1"
+		wget --https-only -nv --max-redirect=2 -O "$2" "$1"
 	fi
 }
